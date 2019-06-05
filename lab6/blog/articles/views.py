9149242aed20089c0be4bce829426e1a286fd34a
purrplestorm <47472044+purrplestorm@users.noreply.github.com>
2019-06-05 08:31:07 +0300

    Add files via upload

---
+++ lab6/blog/articles/views.py
@@ -0,0 +1,17 @@
+from articles import models
+from django.shortcuts import render
+from articles.models import Article
+from django.http import HttpResponse
+from django.http import Http404
+
+def archive(request):
+    return render(request, 'archive.html', {"posts": Article.objects.all()})
+
+def get_article(request, article_id):
+    try:
+        post = Article.objects.get(id=article_id)
+        return render(request, 'article.html', {"post": post})
+    except Article.DoesNotExist:
+        raise Http404
+
+
